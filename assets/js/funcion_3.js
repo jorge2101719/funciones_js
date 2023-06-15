759e8d763ac5ee55efbe89c52f2f7db5323f4a97
jorge2101719 <rusosky01@gmail.com>
2023-06-14 21:34:45 -0400

    se agregar un tercer script y da código para cambiar color de fondo

---
+++ assets/js/funcion_3.js
@@ -0,0 +1,6 @@
+// Parte 3: modificación de una función
+
+let ele = document.getElementById('ele1');
+ele.addEventListener('click', function pintar() {
+    ele.style.backgroundColor = 'yellow';
+})
