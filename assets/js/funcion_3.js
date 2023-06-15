987300e721de0be15dcf2a3f2b4c8787fd4a7af3
jorge2101719 <rusosky01@gmail.com>
2023-06-14 22:23:48 -0400

    se elabora lógica del punto 3.2

--- assets/js/funcion_3.js
+++ assets/js/funcion_3.js
@@ -1,6 +1,23 @@
 // Parte 3: modificación de una función
 
-let ele = document.getElementById('ele1');
-ele.addEventListener('click', function pintar() {
-    ele.style.backgroundColor = 'yellow';
+// 3.1
+
+let ele1 = document.getElementById('ele1');
+
+
+ele1.addEventListener('click', function () {
+    console.log('función 3.1')
+    ele1.style.backgroundColor = 'yellow';
 })
+
+
+
+// 3.2
+
+function pintar (color = 'green') {
+    // let ele2 = document.querySelector('#ele2');
+    ele2.style.backgroundColor = color;
+}
+
+
+// 3.3
