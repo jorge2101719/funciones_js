6fc3c83121478527caea5e97c8979313b4f025f2
jorge2101719 <rusosky01@gmail.com>
2023-06-15 13:41:01 -0400

    se traspasan los puntos 3.1 y 3.2 al html dejando solo 3.3 en archivo aparte

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,25 @@
 
 // 3.1
 
+/*
 let ele1 = document.getElementById('ele1');
 
 ele1.addEventListener('click', function () {
     console.log('función 3.1')
     ele1.style.backgroundColor = 'yellow';
 })
+*/
 
 
 
 // 3.2
 
+/*
 function pintar (color = 'green') {
     // let ele2 = document.querySelector('#ele2');
     ele2.style.backgroundColor = color;
 }
+*/
 
 
 // 3.3
