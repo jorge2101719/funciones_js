77d78b31777c1bfff2b3a77edd87db1475d7704d
jorge2101719 <rusosky01@gmail.com>
2023-06-14 21:25:09 -0400

    estructura básica y soluciones de los dos primeros puntos del desafío

---
+++ assets/js/funcion_1.js
@@ -0,0 +1,16 @@
+// Punto 1 del desasfío
+
+// función de declaración
+/*
+function example (a, b, c) {
+    return a + b + c;
+}
+*/
+
+// función de expresión
+const example = function (a, b,c) {
+    return a + b + c;
+}
+
+let resultado = example(1,2,3)
+console.log('el resultado es: ' + resultado)
---
+++ assets/js/funcion_2.js
@@ -0,0 +1,11 @@
+// Parte 2 del desafío
+
+// transformar a función flecha
+/* 
+const suma = function (a, b) {
+    return a + b;
+}
+*/
+const suma = (a, b) => a + b;
+
+console.log('segunda parte: 5 + 7 = ' + suma(5, 7));
